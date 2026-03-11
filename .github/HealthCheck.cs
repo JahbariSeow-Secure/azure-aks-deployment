47eda8593a77abdfa3cfbfa103f40ea0eabe2992
Jahbari Seow <jbseow0@gmail.com>
2026-03-11 00:51:14 -0400

    Add HealthCheck function for HTTP health checks

--- .github/HealthCheck.cs
+++ .github/HealthCheck.cs
@@ -1,1 +1,16 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 
+public class HealthCheck
+{
+    [Function("HealthCheck")]
+    public HttpResponseData Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")]
+        HttpRequestData req)
+    {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.WriteString("OK");
+        return response;
+    }
+}
